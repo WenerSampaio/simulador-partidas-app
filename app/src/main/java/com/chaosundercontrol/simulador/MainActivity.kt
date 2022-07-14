3002ae1face7841de1e6d078dfa3606294f66f61
WenerSampaio <wenersampaio@gmail.com>
2022-07-13 23:17:11 -0300

    botões e cores

--- app/src/main/java/com/chaosundercontrol/simulador/MainActivity.kt
+++ app/src/main/java/com/chaosundercontrol/simulador/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.chaosundercontrol.simulador
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.chaosundercontrol.simulador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val comFindViewById = 1
+        val comBinding = 2
+        val escolha = comFindViewById
+
+        if (escolha == comFindViewById) {
+            //método 1 - usando findViewById, mais repetitivo, evitar
+            setContentView(R.layout.activity_main)
+            val tvHello = findViewById<TextView>(R.id.textViewHello)
+            tvHello.text = "Olá Mundo com findViewById"
+        }
+        if (escolha == comBinding) {
+            //método 2 - recomendado - inserir campos no arquivo build.grade(app)
+            val binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            binding.textViewHello.text = "Olá Mundo com Binding"
+        }
     }
 }
